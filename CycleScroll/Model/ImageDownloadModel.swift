12556ef746332b239a6080c557b4223007906921
dulingkang <dulingkang@163.com>
2015-11-20 23:15:56 +0800

    add imageDownloadModel

---
+++ CycleScroll/Model/ImageDownloadModel.swift
@@ -0,0 +1,13 @@
+//
+//  ImageDownloadModel.swift
+//  CycleScroll
+//
+//  Created by dulingkang on 20/11/15.
+//  Copyright © 2015 shawn. All rights reserved.
+//
+
+import UIKit
+
+class ImageDownloadModel: NSObject {
+
+}
