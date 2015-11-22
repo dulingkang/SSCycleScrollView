2403e56303c2134f60628e43c1e8f4d0f3b8b9f2
dulingkang <dulingkang@163.com>
2015-11-22 10:29:18 +0800

    add download manager

--- CycleScroll/CustomClasses/SSDownloadManager.swift
+++ CycleScroll/CustomClasses/SSDownloadManager.swift
@@ -8,10 +8,38 @@
 
 import Foundation
 
+public protocol URLStringConvertible {
+    var URLString: String { get }
+}
+
+extension String: URLStringConvertible {
+    public var URLString: String {
+        return self
+    }
+}
+
+extension NSURL: URLStringConvertible {
+    public var URLString: String {
+        return absoluteString
+    }
+}
+
 class SSDownloadManager: NSObject {
     
-    func requestJson(urlStr: String, responseJson: ((Bool) -> Void)?) {
-        
+    class var sharedInstance: SSDownloadManager {
+        struct Singleton {
+            static let instance = SSDownloadManager()
+        }
+        return Singleton.instance
+    }
+    
+    func ephemeralRequest(urlStr: URLStringConvertible, complete: ((NSData?, NSURLResponse?, NSError?) -> Void)?) {
+        let sessionConfig = NSURLSessionConfiguration.ephemeralSessionConfiguration()
+        let session = NSURLSession.init(configuration: sessionConfig)
+        let task = session.dataTaskWithURL(NSURL(string: urlStr.URLString)!) { (data, response, error) -> Void in
+            complete!(data, response, error)
+        }
+        task.resume()
     }
 
 }
